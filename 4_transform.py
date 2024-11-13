d7b02b79e041c1b669974273ba5ce4a41406ecb9
Terlekchi Seityaya <terlekchi.seityaya@gmail.com>
2024-11-13 11:44:55 +0300

    Refactor

--- 4_transform.py
+++ 4_transform.py
@@ -10,13 +10,13 @@ import csv
 file_name_in   = "./fits.csv"  # Имя входного файла
 file_name_out  = "./draw.csv"  # Имя выходного файла
 latitude_limit = 65            # Значение крайнего положения широты, максимум 90
-latitude_step  = 10            # Шаг широты
-area_column    = 'AREAS_1'     # Колонка площадей пятен, по которой будет производится построение графика
+latitude_step  = 5             # Шаг широты
+area_column    = 'AREAS_1'     # Колонка площадей пятен, по которой будет производится построение файла
 
 #Открываем файл с данными на чтение
 fd_r = open(file=file_name_in, mode='r')
 #Создаем поток чтения
-fd_r_csv = csv.DictReader(fd_r, delimiter=',', lineterminator='\r\n')
+fd_r_csv = csv.DictReader(fd_r)
 #Переменная для сохранения новой таблицы
 data = list()
 #Вычитываем файл построчно
@@ -27,6 +27,10 @@ for r in fd_r_csv:
     line["DateTime"] = ("{y}.{m}.{d} {H}:{M}:{S}")\
         .format(y=r["YEAR"], m=r["MONTH"] , d=r["DAY"], \
                 H=r["HOUR"], M=r["MINUTE"], S=r["SECOND"])
+    #Читаем значение широты пятна
+    latitude_spot = r["LAT-DE"]
+    #Читаем значение площади пятна
+    area_spot     = r[area_column]
     #Проходим по колонкам широт
     for i in range(latitude_limit * -1, latitude_limit * +1, latitude_step):
         #Определяем диапазон широт
@@ -36,15 +40,12 @@ for r in fd_r_csv:
         latitude = str("{beg}_{end}").format(beg=latitude_beg, end=latitude_end)
         #Записываем ноль в диапазон
         line[latitude] = "0"
-        #Читаем значение широты пятна
-        latitude_spot = r["LAT-DE"]
         #Определям в какую колонку широт необходимо поместить значение
         if(float(latitude_spot) >= latitude_beg and float(latitude_spot) < latitude_end):
-            #Читаем значение площади пятна в колонке и записываем в новую колонку
-            line[latitude] = r[area_column]
+            #Записываем в диапазон площадь пятна
+            line[latitude] = area_spot
     #Добавляем строку в таблицу
     data.append(line)
-
 
 #Открываем файл с результатами на запись
 fd_w = open(file=file_name_out, mode='w+')
@@ -61,8 +62,7 @@ for i in range(latitude_limit * -1, latitude_limit * +1, latitude_step):
     header.append(latitude)
 
 #Создаем поток записи
-fd_w_csv = csv.DictWriter(fd_w, fieldnames=header, delimiter=',', \
-                          lineterminator='\r\n', quoting=csv.QUOTE_ALL)
+fd_w_csv = csv.DictWriter(fd_w, fieldnames=header, quoting=csv.QUOTE_ALL)
 #Записывае заголовок
 fd_w_csv.writeheader()
 #Записываем данные
